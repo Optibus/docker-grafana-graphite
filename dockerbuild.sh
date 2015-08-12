adcf9e4ad49d3fe1ee3beba3ccb8c11efae32654
Assaf Lavie <me@assaflavie.com>
2015-08-12 16:11:41 +0300

    Using Jinja template for Dockerfile

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 source $ROOT/build-tools/build_funcs.sh
-if [[ -z "$IMAGE" ]]; then
-    export IMAGE=ubuntu
+if [[ -z "$IMAGE" ]]; then 
+    export IMAGE=grafana
 fi
+if [[ -z "$FROM_IMAGE" ]]; then 
+    export FROM_IMAGE=ubuntu 
+fi 
 build $@
